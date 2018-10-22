d20d9e8358f9261e7823e8c71d5815f41de1d887
Ivan Zahoranszky <ivan.zahoranszky@transferwise.com>
2018-10-22 21:21:23 +0200

    Initial version

---
+++ app/src/main/java/com/tryit/example/MyService.java
@@ -0,0 +1,65 @@
+package com.tryit.example;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+
+    private Timer timer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        timer = new Timer();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My Awesome App")
+                .setContentText("Doing some work...")
+                .setContentIntent(pendingIntent).build();
+
+        startForeground(1337, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Log.i("Ivan", "tick");
+            }
+
+        }, 0, 1000);
+
+        super.onStartCommand(intent, flags, startId);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
+    }
+}
